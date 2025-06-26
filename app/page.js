43394e0ea39680640deb662de4b8ca1fd6120659
Bhavya Kotapati <bhavyakotapati4903@gmail.com>
2025-06-26 17:23:07 +0530

    Updated Home page

--- app/page.js
+++ app/page.js
@@ -1,16 +1,14 @@
-import styles from './page.module.css';
-
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
       <h1>Welcome to Grandmatales</h1>
       <p>Explore the pages:</p>
       <ul>
         <li><a href="/terms">Terms</a></li>
         <li><a href="/faq">FAQ</a></li>
-        <li><a href="/optout">Opt‑Out</a></li>
-        <li><a href="/engage">Post‑Purchase Engagement</a></li>
+        <li><a href="/optout">Opt-Out</a></li>
+        <li><a href="/engage">Post-Purchase Engagement</a></li>
       </ul>
-    </div>
+    </main>
   );
 }
