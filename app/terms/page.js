5526d282136cf56a4a34b6d58ed386270acc1fea
Bhavya Kotapati <bhavyakotapati4903@gmail.com>
2025-06-27 17:51:18 +0530

    Cleaned Module 6 repo

--- app/terms/page.js
+++
@@ -1,8 +0,0 @@
-export default function TermsPage() {
-  return (
-    <div style={{ fontFamily: "sans-serif'", padding: "2rem" }}>
-      <h1>Terms and Conditions</h1>
-      <p>PLACEHOLDER – DO NOT EDIT</p>
-    </div>
-  );
-}
